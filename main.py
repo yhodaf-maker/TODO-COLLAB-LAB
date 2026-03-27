27a6c80309679c8d791a9cca078766753758a23d
Yehuda feder <yhodafeder@gmail.com>
2026-03-27 18:29:40 +0300

    another task

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from todo import add_task, list_tasks, remove_last_task, update_first_task,count
 add_task("Initial task by A")
 add_task("B: buy groceries")
 add_task("A: clean room")
+add_task("A iteration task")
 tasks = list_tasks()
 for t in tasks:
     print(t)
